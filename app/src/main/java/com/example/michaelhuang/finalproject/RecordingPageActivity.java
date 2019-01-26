e1803c25bd9ed3f289e89d1751a4d98b394e195e
michael huang <michaelhuang@michaels-MacBook-Pro.local>
2019-01-25 21:29:11 -0500

    add recording video feature

--- app/src/main/java/com/example/michaelhuang/finalproject/RecordingPageActivity.java
+++ app/src/main/java/com/example/michaelhuang/finalproject/RecordingPageActivity.java
@@ -1,20 +1,45 @@
 package com.example.michaelhuang.finalproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class RecordingPageActivity extends AppCompatActivity {
 
+    private static int VIDEO_REQUEST =101;
+    private Uri videoUri = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recording_page);
+
+        Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if(videoIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(videoIntent,VIDEO_REQUEST);
+        }
+
     }
 
-    public void stop(View view){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode==VIDEO_REQUEST && resultCode ==RESULT_OK){
+            videoUri = data.getData();
+        }
+    }
+
+    public void result(View view){
         Intent resultPage = new Intent(this, ResultActivity.class);
+        resultPage.putExtra("videoUri",videoUri.toString());
         startActivity(resultPage);
+    }
+
+    public void recordAgain(View view){
+        Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if(videoIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(videoIntent,VIDEO_REQUEST);
+        }
     }
 }
--- app/src/main/java/com/example/michaelhuang/finalproject/ResultActivity.java
+++ app/src/main/java/com/example/michaelhuang/finalproject/ResultActivity.java
@@ -1,15 +1,32 @@
 package com.example.michaelhuang.finalproject;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class ResultActivity extends AppCompatActivity {
+    ArrayList<Bitmap> frameList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_page);
+
+        Uri videoUri = Uri.parse(getIntent().getExtras().getString("videoUri"));
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(this,videoUri);
+        frameList = new ArrayList<Bitmap>();
+        for(int i=0;i<3;i++) {
+            Bitmap bitmap = retriever.getFrameAtTime(i*1000000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+            frameList.add(bitmap);
+        }
+
+
     }
 
     public void addMore(View view){
